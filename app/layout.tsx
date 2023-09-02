844d0ce373b0df8516cac159339e830255cb0e64
Owen McComas <owen@mccomas.me>
2023-09-02 00:15:17 -0700

    Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { cn } from "@/lib/utils"
 import { SiteHeader } from "@/components/site-header"
 import { TailwindIndicator } from "@/components/tailwind-indicator"
 import { ThemeProvider } from "@/components/theme-provider"
+import { Analytics } from '@vercel/analytics/react'
 
 export const metadata: Metadata = {
   title: {
@@ -32,6 +33,7 @@ interface RootLayoutProps {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <>
+    <Analytics />
       <html lang="en" suppressHydrationWarning>
         <head />
         <body
